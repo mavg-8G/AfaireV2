13aaefa3051a08a8dba2bd375c8f47d01693cd7f
Matias Vasconez <MatiasVasconez9@gmail.com>
2025-07-02 15:41:37 +0000

    make sure to add the header X-Content-Type-Options to nosniff

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,6 +28,7 @@ export function middleware(request: NextRequest) {
   response.headers.set('Content-Security-Policy', cspHeader);
 
   // Other security headers
+  response.headers.set('X-Content-Type-Options', 'nosniff');
   response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
   response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
   response.headers.set('Permissions-Policy', "camera=(), microphone=(), geolocation=(), payment=(), usb=(), display-capture=()");
